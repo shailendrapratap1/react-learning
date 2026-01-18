25e7cf7bd42eb1f0b7403240972bc8a8299c6e9d
shailendra pratap <singhlalprat@gmail.com>
2026-01-18 19:08:03 +0530

    Update Navbar.jsx

--- Day2/my-project/src/pages/Navbar.jsx
+++ Day2/my-project/src/pages/Navbar.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React,{useState} from 'react'
 import { Button } from '../Components'
-import { MoveRightIcon } from 'lucide-react'
+import { MoveRightIcon ,Menu,X} from 'lucide-react'
 const Navbar = () => {
+      const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className='fixed top-0 left-0 right-0 h-30 bg-amber-50 flex items-center justify-between px-8 z-100'>
      
@@ -13,8 +14,27 @@ const Navbar = () => {
      <li className='text-black font-medium transition-colors hover:text-gray-400 list-none '>Community</li>
      <li className='text-black font-medium transition-colors hover:text-gray-400 list-none'>Blog</li>
      <li className='text-black font-medium transition-colors hover:text-gray-400 list-none'>Pricing</li>
-     <Button className='bg-lime-500'>Register now</Button>
+     <Button className='text-lime-500 bg-green-400 h-15 w-45 '>Register now <MoveRightIcon /></Button>
+    
+    
     </div>
+      <button className='block md:hidden text-sky-500   cursor-pointer' onClick={()=>setIsOpen(!isOpen)}>
+      {isOpen ? <X /> : <Menu />}
+      </button>
+    
+    {isOpen &&(
+        <div className='absolute top-15 left-0 right-0 bg-black text-white flex flex-col gap-4 md:hidden'>
+     <li className=' font-medium transition-colors hover:text-gray-400 list-none' onClick={()=>setIsOpen(false)}>Home</li>
+          <li className=' font-medium transition-colors hover:text-gray-400 list-none' onClick={()=> setIsOpen(false)}>features</li>
+     <li className=' font-medium transition-colors hover:text-gray-400 list-none ' onClick={()=> setIsOpen(false)}>Community</li>
+     <li className=' font-medium transition-colors hover:text-gray-400 list-none' onClick={()=>setIsOpen(false)}>Blog</li>
+     <li className=' font-medium transition-colors hover:text-gray-400 list-none' onClick={()=> setIsOpen(false)}>Pricing</li>
+          <Button className='text-lime-500 bg-green-400 h-15 w-45 '>Register now <MoveRightIcon className='size-5 'onClick={()=> setIsOpen(false)}/></Button>
+
+        </div>
+    )
+    }
+    
     </nav>
   )
 }
